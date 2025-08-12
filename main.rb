01b565285807bd9c69ff28a4f096e726148c921e
0x1eef <0x1eef@protonmail.com>
2025-08-12 00:59:57 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -4,22 +4,27 @@ require "erb"
 llm   = LLM.openai(key: ENV["OPENAI_SECRET"])
 bot   = LLM::Bot.new(llm)
 docs  = Dir["documents/*.pdf"]
-files = docs.map { llm.files.create(file: _1) }
+files = docs.map do
+  print "[-] wait ", _1, "\n"
+  llm.files.create(file: _1)
+end
 store = llm.vector_stores.create(name: "FreeBSD Handbook", file_ids: files.map(&:id))
 
+print "[-] wait vector store", "\n"
 while store.status != "completed"
   sleep(0.5)
   store = llm.vector_stores.get(vector: store)
 end
+print "[-] done!", "\n"
 
 loop do
   print "> "
-  question = $stdin.gets.chomp
-  res = llm.vector_stores.search(vector: store, query: question)
+  query = $stdin.gets&.chomp || break
+  res = llm.vector_stores.search(vector: store, query:)
   chunks = res.data.select { _1.score > 0.7 }
   bot.chat(stream: $stdout) do |prompt|
     prompt.system ERB.new(File.read("prompts/system.erb.txt")).result(binding)
-    prompt.user(question)
+    prompt.user(query)
   end.to_a
   print "\n"
 rescue LLM::Error => e
